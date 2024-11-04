6be79e7c39cb8790d5a8e73e975ec0c63bc20f5e
Lee Yeon <rofttm0033@gmail.com>
2024-11-04 16:03:56 +0900

    Initial commit

---
+++ src/main/java/umc/study/domain/Region.java
@@ -0,0 +1,22 @@
+package umc.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.study.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Region extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(20)")
+    private String name;
+
+
+}
