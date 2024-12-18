4bd222d535683e3d2ea34eceec6ba34812bd12d7
Lee Yeon <rofttm0033@gmail.com>
2024-12-18 20:12:17 +0900

    fix: ci/cd pipeline fixing

--- src/test/java/umc/ApplicationTests.java
+++ src/test/java/umc/ApplicationTests.java
@@ -2,8 +2,12 @@ package umc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import umc.study.StudyApplication;
 
 @SpringBootTest
+@ContextConfiguration(classes = {StudyApplication.class})
+
 class ApplicationTests {
 
 	@Test
